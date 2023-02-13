1b143ef99ddc667f4de4e0bab16d5c7b7a86a222
mohamed <m635379@gmail.com>
2023-02-13 12:23:53 +0200

    add navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <Container>
+        <Row className="justify-content-between w-100 align-items-center">
+          <Col sm={2} className="logo">
+            <img
+              src="https://e1.pngegg.com/pngimages/974/699/png-clipart-movies-and-popcorn-folder-icon-movies.png"
+              alt="logo"
+              className="rounded-circle"
+            />
+          </Col>
+          <Col sm={10} className="input-search">
+            <input
+              type="text"
+              className="form-control border-0"
+              placeholder="Search a movie..."
+            />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Navbar;
